6b4114ced9c1e8f0b3120427dc033b4ea7c5c5c0
Emmanuel Leakono <leakonoemmanuel3@gmail.com>
2024-03-14 15:00:47 +0300

    Arrays

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Write your solution here!
+const cats=["Milo","Otis","Garfield"]
+function destructivelyAppendCat(Ralph){
+    cats.push(Ralph)
+}
+function destructivelyPrependCat(Bob){
+    cats.unshift(Bob)
+}
+function   destructivelyRemoveLastCat(Garfield){
+    cats.pop(Garfield)
+}
+function destructivelyRemoveFirstCat(Milo){
+    cats.shift(Milo)
+}
+function appendCat(name){
+    return [...cats, name ]
+ }
+ 
+ 
+ function prependCat(name){
+     return [name ,...cats ]
+ }
+ 
+ function removeLastCat(){
+     return cats.slice(0, cats.length - 1);
+ }
+ 
+ function removeFirstCat(){
+     return cats.slice(1);
+ }
+
